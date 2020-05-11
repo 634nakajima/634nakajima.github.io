199adadf358c621a4a53eb4c1cae8be3f3f553ec
634nakajima <634.nakajima@gmail.com>
2020-05-11 10:40:59 +0900

    create template

--- webslides/template/template.js
+++ webslides/template/template.js
@@ -1,6 +1,49 @@
-/*JSONファイル読み込み-----------------------------------------------------*/
+/*template要素の書き出し--------------------------------------------------*/
+document.write(
+  //目次へ戻るバナー
+  '<template id="bannerTemplate">',
+  '<header role="banner">',
+  '<nav role="navigation">',
+  '<a href="index.html#slide=2" title="目次へ戻る">',
+  '&lt; 目次へ戻る',
+  '</a>',
+  '</nav>',
+  '</header>',
+  '</template>',
+  //スライド上部のヘッダ
+  '<template id="slideHeaderTemplate">',
+  '<p class="text-context"></p><br>',
+  '</template>',
+  //授業タイトル
+  '<template id=titleTemplate>',
+  '<div class="aligncenter">',
+  '<h2 class="text-landing"><strong></strong></h2>',
+  '<h2 class="text-landing"><strong></strong></h2>',
+  '<hr>',
+  '</div>',
+  '</template>',
+  //授業内容リスト
+  '<template id="listTemplate">',
+  '<div class="aligncenter">',
+  '<ol>',
+  '<li>',
+  '<h5><a></a></h5>',
+  '</li>',
+  '</ol>',
+  '</div>',
+  '</template>'
+);
+/*----------------------------------------------------------------------*/
+
+/*JSONの読み込みと授業回の取得----------------------------------------------*/
 const slideData = readJSON("./contents.json");
 var thisorder = document.querySelector('article').getAttribute("order");
+/*----------------------------------------------------------------------*/
+
+/*目次へ戻るバナー---------------------------------------------------------*/
+var bannerTemplate = document.querySelector('#bannerTemplate').content;
+var bannerElement = document.getElementById("banner");
+bannerElement.appendChild(document.importNode(bannerTemplate, true));
 /*----------------------------------------------------------------------*/
 
 /*スライド上部のヘッダ-----------------------------------------------------*/
@@ -10,8 +53,8 @@ var slideHeaderElements = document.getElementsByClassName("slideHeader");
 for (let i = 0; i < slideHeaderElements.length; i++) {
   const fragment = document.createDocumentFragment();// フラグメント
   const clone = document.importNode(slideHeaderTemplate, true);// テンプレートのノードを複製
-  const maintitle = clone.querySelector('p');// テンプレート内のp要素
-  maintitle.innerHTML = slideData.maintitle + '</br >' + "第 " + slideData[thisorder].number + " 回 " + slideData[thisorder].subtitle;// テンプレートの要素に適用する
+  const classtitle = clone.querySelector('p');// テンプレート内のp要素
+  classtitle.innerHTML = slideData.maintitle + '</br >' + "第 " + slideData[thisorder].number + " 回 " + slideData[thisorder].classtitle;// テンプレートの要素に適用する
   fragment.appendChild(clone);// 複製したノードをフラグメントに挿入
   slideHeaderElements.item(i).appendChild(fragment);
 }
@@ -26,43 +69,60 @@ for (let i = 0; i < classTitleElements.length; i++) {
   const clone = document.importNode(classTitleTemplate, true);// テンプレートのノードを複製
   const titles = Array.from(clone.querySelectorAll('strong'));// テンプレート内のh5要素
   titles[0].innerHTML = "第" + slideData[thisorder].number + "回";
-  titles[1].innerHTML = slideData[thisorder].subtitle;// テンプレートの要素に適用する
+  titles[1].innerHTML = slideData[thisorder].classtitle;// テンプレートの要素に適用する
   fragment.appendChild(clone);// 複製したノードをフラグメントに挿入
   classTitleElements.item(i).appendChild(fragment);
 }
 /*----------------------------------------------------------------------*/
 
-/*授業内容リスト----------------------------------------------------------*/
+/*授業チャプターリスト------------------------------------------------------*/
 var chapterTemplate = document.querySelector('#listTemplate').content;
-var chapterElements = document.getElementsByClassName("classChapter");
+var chapterElements = document.getElementsByClassName("chapterList");
 const sections = Array.from(document.querySelectorAll('section'));
 
 // それぞれHTMLに挿入
 for (let i = 0; i < chapterElements.length; i++) {
   const fragment = document.createDocumentFragment();// フラグメント
   const clone = document.importNode(chapterTemplate, true);// テンプレートのノードを複製
-  fragment.appendChild(clone.querySelector('ol'));// テンプレート内のol要素
+
+  fragment.appendChild(clone);// テンプレート内のol要素
   const ol = fragment.querySelector('ol');//fragment内のol要素
   ol.className = "chapter";
-  console.log(sections.length);
-  for (part of slideData[thisorder].part) {
-    partClone = document.importNode(ol.querySelector('li'), true);
-    const partTitle = partClone.querySelector('a');// テンプレート内のa要素
-    partTitle.textContent = part.title;//授業内容を代入
+  for (chapter of slideData[thisorder].chaptertitle) {
+    chapterClone = document.importNode(ol.querySelector('li'), true);
+    const chapterTitle = chapterClone.querySelector('a');// テンプレート内のa要素
+    chapterTitle.textContent = chapter;//授業内容を代入
 
     //授業パートトップスライドへのリンク
-    const partIndex = sections.findIndex(({ className }) => (className.split(' ')[0].slice(4) - 1) === (slideData[thisorder].part.indexOf(part)));
-    const titleIndex = sections.indexOf(chapterElements[0].closest("section"));
-    const correctedIndex = (sections.length / 2 + partIndex - titleIndex + 1) % (sections.length / 2);
-    partTitle.href = "#slide=" + correctedIndex;
-    ol.appendChild(partClone);// 複製したノードをフラグメントに挿入
+    const chapterIndex = 1 + sections.findIndex(({ className }) => (
+      className.split(' ')[0].slice(String("chapter").length) - 1)
+      === (slideData[thisorder].chaptertitle.indexOf(chapter))
+    );
+    chapterTitle.href = "#slide=" + chapterIndex;
+    ol.appendChild(chapterClone);// 複製したノードをフラグメントに挿入
   }
+
   fragment.querySelector('li').remove();//テンプレートのliを削除
   chapterElements.item(i).appendChild(fragment);
 }
 /*----------------------------------------------------------------------*/
 
-/*授業内容タイトル------------------------------------------------------------*/
+/*授業チャプタータイトル----------------------------------------------------*/
+var chapterTitleTemplate = document.querySelector('#titleTemplate').content;
+var chapterTitleElements = document.getElementsByClassName("chapterTitle");
+
+for (let i = 0; i < chapterTitleElements.length; i++) {
+  const fragment = document.createDocumentFragment();// フラグメント
+  const clone = document.importNode(chapterTitleTemplate, true);// テンプレートのノードを複製
+  const titles = Array.from(clone.querySelectorAll('strong'));// テンプレート内のh5要素
+  const chapterNumber = chapterTitleElements.item(i).closest("section").className.split(' ')[0].slice(String("chapter").length) - 1;
+  titles[1].innerHTML = slideData[thisorder].chaptertitle[chapterNumber];// テンプレートの要素に適用する
+  fragment.appendChild(clone);// 複製したノードをフラグメントに挿入
+  chapterTitleElements.item(i).appendChild(fragment);
+}
+/*----------------------------------------------------------------------*/
+
+/*科目パートタイトル-------------------------------------------------------*/
 var partTitleTemplate = document.querySelector('#titleTemplate').content;
 var partTitleElements = document.getElementsByClassName("partTitle");
 
@@ -70,8 +130,8 @@ for (let i = 0; i < partTitleElements.length; i++) {
   const fragment = document.createDocumentFragment();// フラグメント
   const clone = document.importNode(partTitleTemplate, true);// テンプレートのノードを複製
   const titles = Array.from(clone.querySelectorAll('strong'));// テンプレート内のh5要素
-  const partNumber = partTitleElements.item(i).closest("section").className.split(' ')[0].slice(4) - 1;
-  titles[1].innerHTML = slideData[thisorder].part[partNumber].title;// テンプレートの要素に適用する
+  const partNumber = partTitleElements.item(i).closest("section").className.split(' ')[0].slice(String("part").length) - 1;
+  titles[1].innerHTML = slideData.parttitle[partNumber];// テンプレートの要素に適用する
   fragment.appendChild(clone);// 複製したノードをフラグメントに挿入
   partTitleElements.item(i).appendChild(fragment);
 }
